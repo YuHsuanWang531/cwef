7f6751557530c1ae4f50c67a8388bdd77e86500d
YuHsuanWang531 <44403391+YuHsuanWang531@users.noreply.github.com>
2019-05-27 10:39:42 +0800

    Add files via upload

---
+++ 2019/js/cwef.js
@@ -0,0 +1,251 @@
+$(function(){
+	"use strict";
+	// Start of use strict
+	$('a.page-scroll[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - 150)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+	// Closes responsive menu when a scroll trigger link is clicked
+	var width = $(window).width(), height = $(window).height();
+	var iPhone = (navigator.userAgent.match(/iPhone/i) != null) || (navigator.userAgent.match(/iPod/i) != null);
+	var android = (navigator.userAgent.match(/android/i) != null);
+	var iPad = navigator.userAgent.match(/iPad/i) != null;
+	if ( (width / height) >= 0.5625) {
+		$("video").addClass("landscape");
+	} else {
+		$("video").addClass("portrait");
+	}
+	// opening影片
+	if (width > 769) {
+		$("video").attr('poster', 'img/2019cwef-summer-opening-pc.jpg');
+		$('video source.mp4').attr('src', 'img/2019cwef-summer-opening-pc.mp4');
+	};
+	if (iPad) {
+		iPhone = false;
+		android = false;
+		$('video').load();
+		$("video").attr('poster', 'img/2019cwef-summer-opening-pad.jpg');
+		$('video source.mp4').attr('src', 'img/2019cwef-summer-opening-pad.mp4');
+	}
+	if (iPhone) {
+		iPad = false;
+		android = false;
+		$('video').load();
+		$("video").attr('poster', 'img/2019cwef-summer-opening-mobile.jpg');
+		$('video source.mp4').attr('src', 'img/2019cwef-summer-opening-mobile.mp4');
+	}
+	if (android) {
+		iPad = false;
+		iPhone = false;
+		var video = document.querySelector('video');
+		$('video').load();
+		$("video").attr('poster', 'img/2019cwef-summer-opening-mobile.jpg');
+		$('video source.mp4').attr('src', 'img/2019cwef-summer-opening-mobile.mp4');
+		window.addEventListener('touchstart', function videoStart() {
+			video.play();
+			this.removeEventListener('touchstart', videoStart);
+		});
+	}
+	if ( width < 769 ) {
+		$('.page-scroll').click(function() {
+			$('.navbarDiv').slideUp();
+			$('.wrapper-menu').removeClass('open');
+		});
+		$('.wrapper-menu').click(function() {
+			$('.navbarDiv').slideToggle();
+		});
+	}
+	$('a.more').click(function() {
+		$(this).slideUp();
+		$('.moreDisplay').slideDown();
+		$('.note').addClass('active');
+	});
+	$('a.trend-more-btn').click(function() {
+		//$(this).slideUp();
+		//$('.trend-mobile-none').slideDown();
+		//$('.note').addClass('active');
+		$(this).slideUp();
+		$('.trend-mobile-none').addClass('trend-mobile-none-active');
+	});
+	if ( width >= 769 ){
+		var containerWidth = $('.container').width();
+		var btnLeft = ($(window).width() - containerWidth ) /2 ;
+		$('#btnTop').css({'right':btnLeft});
+	}
+	var btnTopSpeed = 200;
+	$(window).scroll(function(){
+		$(window).scroll(function(){
+			var scrollTop = $(document).scrollTop();
+			if ( width >= 769 ){
+				var windowH = $(window).height() - 130;
+			} else {
+				var windowH = $(window).height() - 80;
+			}
+			$('#btnTop').stop();
+			$('#btnTop').animate({
+				'top' : scrollTop + windowH + 'px'
+			},{queue:true,duration:500});
+			if(scrollTop > 200){
+				$('#btnTop a').fadeIn(btnTopSpeed);
+			}else if(scrollTop < 200){
+				$('#btnTop a').fadeOut(btnTopSpeed);
+			};
+		});
+	});
+	// Collapse Navbar
+	var navbarCollapse = function() {
+		if ($("#mainNav").offset().top > 100) {
+			$("#mainNav").addClass("navbar-shrink");
+		} else {
+			$("#mainNav").removeClass("navbar-shrink");
+		}
+	};
+	var wrapperMenu = document.querySelector('.wrapper-menu');
+	wrapperMenu.addEventListener('click', function(){
+		wrapperMenu.classList.toggle('open');
+	})
+	// Collapse now if page is not at top
+	navbarCollapse();
+	// Collapse the navbar when page is scrolled
+	$(window).scroll(navbarCollapse);
+	if (!location.href.match('review|report|download|ppt')) {
+		new WOW().init();
+		// Activate scrollspy to add active class to navbar items on scroll
+		$('body').scrollspy({
+			target: '#mainNav',
+			offset: 155
+		});
+		$('.popup-with-zoom-anim').magnificPopup({
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+		var speakerNo;
+		$(".popup-with-zoom-anim").click(function () {
+			speakerNo = $(this).attr('id');
+			for (var i = 0; i < data.length; i++) {
+				if (speakerNo == data[i]["speakerNo"]) {
+					$("#relate").hide();
+					$("#speakerImg").attr("src", "img/speaker/" + "" + data[i]["speakerImg"] + ".jpg");
+					$("#speakerImg").attr("alt", "" + data[i]["speakerName"] + "");
+					$("#speakerName").text("" + data[i]["speakerName"] + "");
+					$("#speakerInfo").html("" + data[i]["speakerInfo"] + "");
+					$("#speakerIntro").html("" + data[i]["speakerIntro"] + "");
+					if (data[i]["relateTitle"] !== "") {
+						$("#relate").show();
+						$("#relateLink").text("" + data[i]["relateTitle"] + "");
+						$("#relateLink").attr("href", "" + data[i]["relateLink"] + "");
+					}
+				}
+			}
+			return false;
+		});
+		if ( width > 768 ) {
+			$('.slide-desktop').owlCarousel({
+				animateOut: 'fadeOut',
+				margin: 0,
+				loop: true,
+				nav: true,
+				dots: false,
+				responsiveClass: true,
+				autoplay: true,
+				autoplayTimeout: 4000,
+				autoplayHoverPause: true,
+				smartSpeed: 600,
+				items: 1,
+				animateIn: "zoomIn",
+			});
+		} else {
+			$('.slide-mobile').owlCarousel({
+				animateOut: 'fadeOut',
+				margin: 0,
+				loop: true,
+				nav: true,
+				dots: false,
+				responsiveClass: true,
+				autoplay: true,
+				autoplayTimeout: 4000,
+				autoplayHoverPause: true,
+				smartSpeed: 600,
+				items: 1
+			});
+		}
+	}
+	$('.inputBox i').click(function(){
+		$(this).siblings('input').attr('type',
+			$(this).siblings('input').attr('type')==='password'?'text':'password'
+		);
+		$(this).toggleClass('see');
+	});
+	function countdown() {
+		var now = new Date();
+		var eventDate = new Date(2019, 0, 14);
+		var currentTime = now.getTime();
+		var evenTime = eventDate.getTime();
+		var remTime = evenTime - currentTime;
+		var sec = Math.floor(remTime / 1000);
+		var min = Math.floor(sec / 60);
+		var hur = Math.floor(min / 60);
+		var day = Math.floor(hur / 24);
+			hur %= 24;
+			min %= 60;
+			sec %= 60;
+		hur = (hur < 10) ? "0" + hur : hur;
+		min = (min < 10) ? "0" + min : min;
+		sec = (sec < 10) ? "0" + sec : sec;
+		$('#sec').text(sec);
+		$('#min').text(min);
+		$('#hours').text(hur);
+		$('#day').text(day);
+		setTimeout(countdown, 1000);
+	}
+	countdown();
+	//nav
+	if ( width >= 769 ){
+		$("#dropdown1").hover(
+			function(){
+				$("#dropdownList1").stop(true,true).slideToggle(300);
+		});
+		$("#dropdown2").hover(
+		function(){
+			$("#dropdownList2").stop(true,true).slideToggle(300);
+		});
+	} else {
+		$("#dropdown1").on('click',
+		function(){
+			$("#dropdownList1").stop(true,true).slideToggle(300);
+		});
+		$("#dropdown2").on('click',
+			function(){
+				$("#dropdownList2").stop(true,true).slideToggle(300);
+		});
+	}
+	//購票
+		$(".bottom-bar").each(function(){
+		var $window = $(window),
+			//$header = $('.visual .text'),
+			$header = $(this),
+			headerOffsetTop = $header.offset().top;
+		$window.on("scroll",function(){
+			if($window.scrollTop() > headerOffsetTop){
+				$(".bottom-bar").addClass("bar-active");
+			} else {
+				$(".bottom-bar").removeClass("bar-active");
+			}
+		});
+	});
+});
---
+++ 2019/js/speaker-en.js
@@ -0,0 +1,304 @@
+var data = 
+[
+	{
+		"speakerNo": "sk01",
+		"speakerImg": "Tain-Jy-Chen",
+		"speakerName": "Tain-Jy Chen",
+		"speakerFlag": "",
+		"speakerInfo": "Emeritus Professor, Department of Economics, National Taiwan University",
+		"speakerIntro": "A well-known economist, Tain-Jy Chen has previously headed the Cabinet-level National Development Council and the Chung-Hua Institution for Economic Research. He is an expert in international trade and economic development, with a focus on Taiwan-China relations.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk02",
+		"speakerImg": "Chenggang-XU",
+		"speakerName": "Chenggang XU",
+		"speakerFlag": "",
+		"speakerInfo": "Professor of Economics, Cheung Kong Graduate School of Business",
+		"speakerIntro": "The director of CKGSB’s Center for Institutions and AI-Big Data, Chenggang Xu was the first winner (with Yingyi Qian) of the China Economics Prize for his work on incentive mechanisms in China’s economy. He has taught at the London School of Economics and University of Hong Kong. ",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk03",
+		"speakerImg": "Ashok-Kantha",
+		"speakerName": "Ashok K. Kantha",
+		"speakerFlag": "",
+		"speakerInfo": "Director, Institute of Chinese Studies, Delhi/Former Ambassador, India to China",
+		"speakerIntro": "A career diplomat for nearly 40 years, Ashok K. Kantha specializes in Asian Affairs. He played a key role in promoting strategic cooperation with China while serving as India’s ambassador in Beijing until January 2016 and has also worked on issues involving Pakistan, Afghanistan and Iran.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk04",
+		"speakerImg": "Davidd-Liu",
+		"speakerName": "Davidd Liu",
+		"speakerFlag": "",
+		"speakerInfo": "Co-founder , CHOCO Media CO., LIMITED/ CEO,LINE TV",
+		"speakerIntro": "Davidd Liu entered the OTT market three years ago with video streaming platform Choco TV, which has developed its own programming and is now Taiwan’s biggest OTT platform. It was acquired by Line TV in November 2018. Mr. Liu also founded the New Media Entertainment Association.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk05",
+		"speakerImg": "Joseph-Chou",
+		"speakerName": "Joseph Chou",
+		"speakerFlag": "",
+		"speakerInfo": "Chairman & CEO, PricewaterhouseCoopers Taiwan",
+		"speakerIntro": "Joseph Chou specializes in audit and assurance, IPO and market listing services, capital-raising consultations and global capital market advisory services. He provides end-to end solutions to help clients address complex management and accounting issues and fundraising needs.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk06",
+		"speakerImg": "James-Chen",
+		"speakerName": "James Chen",
+		"speakerFlag": "",
+		"speakerInfo": "President & CEO，CTBC Bank Co., Ltd.",
+		"speakerIntro": "James Chen has led CTBC Bank in rapidly internationalizing its operations and strengthening its emphasis on innovative services. He has previously served as president of CTBC Financial Holding, chairman of CTBC Asset Management and vice president of ABN Amro’s Taiwan branch. In 2015, He was awarded 「Best Executive for Taiwan」 by《 Asiamoney》, together with CTBC Financial Holding being awarded 「Best Managed Company in Taiwan – Large Cap」.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk07",
+		"speakerImg": "Huay-Leng-Lee",
+		"speakerName": "Huay Leng Lee",
+		"speakerFlag": "",
+		"speakerInfo": "Head, Chinese Media Group, Singapore Press Holdings Ltd",
+		"speakerIntro": "Huay Leng Lee worked as a journalist and editor for Lianhe Zaobao for 20 years. The group she now heads includes Lianhe Zaobao, Lianhe Wanbao, Shin Min Daily, digital platforms zaobao.sg and zaobao.com, radio stations UFM 100.3 and 96.3 and the Han Culture and Education Group.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk08",
+		"speakerImg": "Hwai-min-LIN",
+		"speakerName": "Hwai-min LIN",
+		"speakerFlag": "",
+		"speakerInfo": "Founder/Artistic Director, Cloud Gate Dance Theatre of Taiwan",
+		"speakerIntro": "Hwai-min LIN founded the Cloud Gate Dance Theatre in 1973 and built it into one of the top contemporary dance groups in the world. He has won countless awards, including the prestigious Samuel H. Scripps/ADF Award for Lifetime Achievement. Lin plans to retire at the end of 2019.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk09",
+		"speakerImg": "Sega-Cheng",
+		"speakerName": "Sega Cheng",
+		"speakerFlag": "",
+		"speakerInfo": "Co-founder & CEO, iKala Interactive Media Inc.",
+		"speakerIntro": "The head of AI-driven marketing technology company iKala, Sega Cheng focuses on online video technology, but sees himself more as a social observer pursuing meaning. He was previously a software engineer at Google, working on Android multimedia, map, and Chinese-search projects.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk10",
+		"speakerImg": "Benson-Wu",
+		"speakerName": "Benson Wu",
+		"speakerFlag": "",
+		"speakerInfo": "Founder, CyCarrier Technology Co., Ltd.",
+		"speakerIntro": "Benson Wu is an experienced entrepreneur and cybersecurity expert who has appeared in such forums as Blackhat 2013, DEFCON, and HITCON (Hacks in Taiwan). Before starting CyCarrier, he was general manager at Verint Systems Taiwan, which acquired Xecure Lab, a firm he co-founded.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk11",
+		"speakerImg": "Sheng-Jean-Huang",
+		"speakerName": "Sheng-Jean Huang",
+		"speakerFlag": "",
+		"speakerInfo": "Superintendent, Taipei City Hospital, Taiwan, R.O.C.",
+		"speakerIntro": "An advocate for the “good death” movement in Taiwan, Sheng-jean Huang was a key backer of Taiwan’s “Patient Right to Autonomy Act,” which takes effect in 2019. In his various posts, he has set up teams to treat or give hospice care to seriously ill patients in their homes or communities.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk12",
+		"speakerImg": "Arthur-Huang",
+		"speakerName": "Arthur Huang",
+		"speakerFlag": "",
+		"speakerInfo": "Founder & CEO, Miniwiz Co., Ltd.",
+		"speakerIntro": "Named a Technology Pioneer by the World Economic Forum in 2015, Arthur Huang is an architect, engineer, and circular economy champion whose company turns waste into innovative products. His goal is to see all products be made from recycled materials and then be recycled themselves.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk13",
+		"speakerImg": "Chen-Fu-Chien",
+		"speakerName": "Chen-Fu Chien",
+		"speakerFlag": "",
+		"speakerInfo": "Tsinghua Chair Professor, National Tsing Hua University Director, Artificial Intelligence for Intelligent Manufacturing Systems (AIMS) Research Center, MOST",
+		"speakerIntro": "A leading authority in Taiwan on Industry 4.0, Chen-Fu Chien has extensive experience in decision science and AI. He has been a key figure in promoting academic cooperation with industry leaders and hidden champions, and backing local businesses in pursuing human-machine collaboration.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk14",
+		"speakerImg": "Roger-Yeh",
+		"speakerName": "Roger C.L. Yeh",
+		"speakerFlag": "",
+		"speakerInfo": "President, Everest Textile Co., LTD",
+		"speakerIntro": "Under Roger C.L. Yeh’s leadership, Everest Textile has become Taiwan’s biggest functional fabric producer, with presences in the U.S., China, Africa, and SE Asia and Nike and Adidas among its clients. Mr. Yeh has made knowledge-intensive manufacturing and ESG practices top priorities.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk15",
+		"speakerImg": "Johnason-Lo",
+		"speakerName": "Johnason Lo",
+		"speakerFlag": "",
+		"speakerInfo": "Founder, JL Design/ Executive Vice President, VFX General Manager, Greater China, Digital Domain",
+		"speakerIntro": "A leader in visual design in Taiwan for nearly 20 years, Johnason Lo has created visual identities for Golden Melody and Golden Pin Design award ceremonies. His work has been recognized internationally, with HBO Asia, Al-Jazeera, Netflix, Disney and CCTV9 among his clients.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk16",
+		"speakerImg": "Yoon-HEO",
+		"speakerName": "Yoon HEO",
+		"speakerFlag": "",
+		"speakerInfo": "Professor, Sogang University, Korea",
+		"speakerIntro": "Yoon Heo specializes in trade liberalization and globalization and currently heads the Korean Association of Trade and Industry Studies. He has advised Korea’s trade minister on FTA negotiations with the U.S. and was formerly a member of Korea’s National Economic Advisory Council.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk17",
+		"speakerImg": "M.K.Lu",
+		"speakerName": "M.K. Lu",
+		"speakerFlag": "",
+		"speakerInfo": "Chairman & CEO, Sino-American Silicon Products Inc.",
+		"speakerIntro": "Dubbed the “King of Acquisitions,” M.K. Lu has participated in over 20 M&As, enabling Taiwanese companies to establish presences around the world. Since Lu assumed his post in 2007, Sino-American’s revenues have grown 10-fold and its market cap has risen above NT$200 billion.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk18",
+		"speakerImg": "Tom-Wang",
+		"speakerName": "Tom Wang",
+		"speakerFlag": "",
+		"speakerInfo": "Writer/ Founder, Start Up Latte",
+		"speakerIntro": "A writer and social entrepreneur, Tom Wang opened the Dream School in 2010 to help companies become more innovative. He founded Start Up Latte in 2016 to inform Chinese-speaking readers of global examples of innovation. He also covers workplace trends and marketing planning.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk19",
+		"speakerImg": "Markus-Wild",
+		"speakerName": "Markus Wild",
+		"speakerFlag": "",
+		"speakerInfo": "Managing Director, EnBW Asia Pacific",
+		"speakerIntro": "Markus Wild is heading the team of Offshore Wind and Electrical Grid experts of EnBW in Taipei. EnBW is one of the leading fully integrated utilities in the EU. Mr. Wild holds an IMBA degree from NCCU. ",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk20",
+		"speakerImg": "Mark-Dooley",
+		"speakerName": "Mark Dooley",
+		"speakerFlag": "",
+		"speakerInfo": "Global Head, Green Investment Group ,Macquarie Capital, Australia",
+		"speakerIntro": "Mark Dooley brings unique expertise to his field, combining his knowledge of investment banking and green energy development gained over the past 25 years. He has led green finance mergers and offshore wind, biomass and solar power projects in Europe and around the world.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk21",
+		"speakerImg": "Ernest-Cu",
+		"speakerName": "Ernest Cu",
+		"speakerFlag": "",
+		"speakerInfo": "President & CEO, Globe Telecom, Inc.",
+		"speakerIntro": "Ernest Cu has led the digital transformation of Globe Telecom and is now redefining the telecom sector by moving into the entertainment sector. He has been named one of the world’s 100 most influential telecom leaders and was hailed as the Philippines’ Best CEO by Finance Asia in 2017.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk22",
+		"speakerImg": "Ming-Yang",
+		"speakerName": "Ming-Yang Yu",
+		"speakerFlag": "",
+		"speakerInfo": "CEO & Co-founder, Rayark Inc.",
+		"speakerIntro": "A co-founder of Taiwanese game developer Rayark Inc., Ming-Yang Yu has designed games such as “Cytus,” “Implosion,” and “Voez” that have received best game awards from Google Play and App Store. He is a firm believer that Taiwan can independently create games that stun the world.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk23",
+		"speakerImg": "Joanna-Catalano",
+		"speakerName": "Joanna Catalano",
+		"speakerFlag": "",
+		"speakerInfo": "CEO, iProspect Asia Pacific",
+		"speakerIntro": "A digital marketing expert, Joanna Catalano previously developed data-led solutions for American Express and Starwood and as head of APAC agency business at Google. She oversees search and performance marketing services at iProspect Asia Pacific, a Dentsu Aegis Network agency.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk24",
+		"speakerImg": "Ethan-Tu",
+		"speakerName": "Ethan Tu",
+		"speakerFlag": "",
+		"speakerInfo": "Founder, Taiwan AILabs Foundation",
+		"speakerIntro": "Ethan Tu first gained fame for founding PTT, Taiwan’s most influential bulletin board. At Microsoft for nearly 15 years, he helped develop virtual assistant Cortana and led AI R&D for Asia-Pacific. He is now building a team to develop AI technology for medical, smart cities and other applications.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk25",
+		"speakerImg": "Kenzo-Nonami",
+		"speakerName": "Kenzo Nonami",
+		"speakerFlag": "",
+		"speakerInfo": "Chairman, Japan Drone Consortium",
+		"speakerIntro": "Known as Japan’s father of drones, Kenzo Nonami built his own lab at Chiba University where he has developed completely autonomous drone technology since 1998. In 2013, he founded ACSL, which has joined with Rakuten to create delivery drones, and took the company public in 2018.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk26",
+		"speakerImg": "Tim-Cranton",
+		"speakerName": "Tim Cranton",
+		"speakerFlag": "",
+		"speakerInfo": "Chief Legal Counsel, Greater China Region, Microsoft",
+		"speakerIntro": "Tim Cranton is responsible for the legal aspects of Microsoft’s cloud computing, internet security, intellectual property protection, compliance, and competition law in Greater China. He previously led Microsoft’s efforts to create systems to combat cybercrimes such as hacker attacks and fraud.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk27",
+		"speakerImg": "Fleury-Perkins",
+		"speakerName": "Cynthia Fleury",
+		"speakerFlag": "",
+		"speakerInfo": "Political Philosophy Researcher and Psychoanalyst Professor, National Conservatory of Arts and Crafts",
+		"speakerIntro": "Cynthia Fleury focuses her political philosophy research on the tools of democratic regulation and has written several books on the subject. As a psychoanalyst, she is the godmother of ICCARRE, a new protocol for AIDS treatment. She is also a member of France’s National Ethics Committee.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk28",
+		"speakerImg": "Andrew-Bruce",
+		"speakerName": "Andrew Bruce",
+		"speakerFlag": "",
+		"speakerInfo": "Head of Global CoE for Value and Health Policy,  Amgen",
+		"speakerIntro": "Andrew Bruce has been engaged in health policy, including insurance systems and technology assessments, throughout his career. Before joining Amgen, he worked at Medicines Australia and was a key member of the team that helped reform Australia’s pricing and reimbursement system.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk29",
+		"speakerImg": "David-Shen",
+		"speakerName": "KUO-JUNG SHEN",
+		"speakerFlag": "",
+		"speakerInfo": "Chairman, Hota Industrial Mfg. Co., Ltd",
+		"speakerIntro": "KUO-JUNG SHEN has transformed Hota, a one-time motorbike parts maker, into a key vendor in Tesla’s supply chain supplying reduction drives and drive shafts. He recently built an AI production line in Chiayi that makes transmission parts for such major car brands as Tesla, BMW, and Chrysler.",
+		"relateTitle": "",
+		"relateLink": ""
+	},
+	{
+		"speakerNo": "sk30",
+		"speakerImg": "TL-Lee",
+		"speakerName": "TL Lee",
+		"speakerFlag": "",
+		"speakerInfo": "General Manager, Wireless Communications BU, MediaTek Inc.",
+		"speakerIntro": "TL Lee oversees smartphone product planning/marketing and strategic partnerships. In 15 years at MediaTek, he has led the development of digital TV demodulator ICs and the world’s first memory-less SoC for feature phones and set up the “Third-party Enabling” department to support major clients.",
+		"relateTitle": "",
+		"relateLink": ""
+	}
+
+]
