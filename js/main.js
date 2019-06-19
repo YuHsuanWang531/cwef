70a715262b3bacd9e88709c4f154415273a46097
YuHsuanWang531 <44403391+YuHsuanWang531@users.noreply.github.com>
2019-06-19 10:06:16 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function() {
+    for (var j = 0; j < sheet.length; j++) {
+        $.getJSON(sheet[j]["json"], function(data) {
+            var htmlv1 = "",
+                htmlv2 = "";
+            for (var i = 0; i < 3; i++) {
+                htmlv1 += "<div class='card'>";
+                htmlv1 += "<span class='card-author'>";
+                if (data.feed.entry[i]["gsx$anonymous"]["$t"] == 1) {
+                    htmlv1 += "匿名";
+                } else {
+                    htmlv1 += data.feed.entry[i]["gsx$company"]["$t"] + "／" + data.feed.entry[i]["gsx$name"]["$t"];
+                }
+                htmlv1 += "</span>";
+                htmlv1 += "<p class='card-text'>";
+                htmlv1 += data.feed.entry[i]["gsx$problem"]["$t"];
+                htmlv1 += "</p>";
+                htmlv1 += "</div>";
+            }
+            for (var i = 3; i < data.feed.entry.length; i++) {
+                htmlv2 += "<div class='card'>";
+                htmlv2 += "<span class='card-author'>";
+                if (data.feed.entry[i]["gsx$anonymous"]["$t"] == 1) {
+                    htmlv2 += "匿名";
+                } else {
+                    htmlv2 += data.feed.entry[i]["gsx$company"]["$t"] + "／" + data.feed.entry[i]["gsx$name"]["$t"];
+                }
+                htmlv2 += "</span>";
+                htmlv2 += "<p class='card-text'>";
+                htmlv2 += data.feed.entry[i]["gsx$problem"]["$t"];
+                htmlv2 += "</p>";
+                htmlv2 += "</div>";
+            }
+            $("main ." + sheet[j]["eng"] + " .card__group--display").html(htmlv1);
+            $("main ." + sheet[j]["eng"] + " .card__group--none").html(htmlv2);
+
+            if (data.feed.entry.length <= 3) {
+                $("main ." + sheet[j]["eng"] + " .card__group > .btn").remove();
+                $("main ." + sheet[j]["eng"] + " .card__group--none").remove();
+            }
+        });
+    }
+
+    $(".tab__nav .anchor").click(function() {
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+        $(".tab__content__pane.active").removeClass("active");
+        $(".tab__content__pane." + $(this).data("pane")).addClass("active");
+    });
+    $(".card__group > .btn").click(function() {
+        $(this).siblings(".card__group--none").slideDown();
+        $(this).slideUp();
+    });
+});
---
+++ js/sheet.js
@@ -0,0 +1,41 @@
+var sheet = [{
+        "chi": "創新的想像 - Thinking and Practice",
+        "eng": "innovation",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/2/public/values?alt=json"
+    },
+    {
+        "chi": "未來醫療 - medical",
+        "eng": "medical",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/3/public/values?alt=json"
+    },
+    {
+        "chi": "未來農食 - farm",
+        "eng": "farm",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/4/public/values?alt=json"
+    },
+    // {
+    //     "chi": "綠色新實踐 - green",
+    //     "eng": "green",
+    //     "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/5/public/values?alt=json"
+    // },
+    {
+        "chi": "金融新實踐 - financial",
+        "eng": "financial",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/6/public/values?alt=json"
+    },
+    {
+        "chi": "城市新實踐 智慧 安全 快樂 - city",
+        "eng": "city",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/7/public/values?alt=json"
+    },
+    {
+        "chi": "生產新實踐 - produce",
+        "eng": "produce",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/8/public/values?alt=json"
+    },
+    {
+        "chi": "創新與科技的跨界應用 - cross",
+        "eng": "cross",
+        "json": "https://spreadsheets.google.com/feeds/list/1KEdxmkteWsHz0U2lMYEICpiqp5-vrSAZWpTmaqz1sLU/9/public/values?alt=json"
+    }
+];
